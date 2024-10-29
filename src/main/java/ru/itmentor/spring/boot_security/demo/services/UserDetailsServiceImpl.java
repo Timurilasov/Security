64d29169865cce43bcda4efaa1ccad63c5b7d507
Timurilasov <ilasovtimur08@gmail.com>
2024-10-29 10:38:53 +0300

    Update UserDetailsServiceImpl.java

--- src/main/java/ru/itmentor/spring/boot_security/demo/services/UserDetailsServiceImpl.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/services/UserDetailsServiceImpl.java
@@ -1,4 +1,5 @@
 package ru.itmentor.spring.boot_security.demo.services;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -8,24 +9,25 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.itmentor.spring.boot_security.demo.model.User;
 import ru.itmentor.spring.boot_security.demo.repositories.UserRepository;
 
-
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
-    private final UserRepository userRepository;
 
-    @Autowired
-    public UserDetailsServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+        private final UserRepository userRepository;
 
-    @Override
-    @Transactional
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
-        if (user == null){
-            throw new UsernameNotFoundException("not found" + username);
+        @Autowired
+        public UserDetailsServiceImpl(UserRepository userRepository) {
+            this.userRepository = userRepository;
         }
 
-        return user;
+        @Override
+        @Transactional
+        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+            System.out.println("Searching for user: " + username); 
+            User user = userRepository.findByUsername(username);
+            if (user == null) {
+                throw new UsernameNotFoundException("User not found with username: " + username);
+            }
+            System.out.println("User found: " + user); 
+            return user; 
+        }
     }
-}
