61e57aceed0555c32a187aee16f958b5f717556a
Timurilasov <ilasovtimur08@gmail.com>
2024-10-29 10:41:03 +0300

    Update UserServicesImpl.java

--- src/main/java/ru/itmentor/spring/boot_security/demo/services/UserServicesImpl.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/services/UserServicesImpl.java
@@ -1,19 +1,22 @@
 package ru.itmentor.spring.boot_security.demo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.itmentor.spring.boot_security.demo.model.Role;
 import ru.itmentor.spring.boot_security.demo.model.User;
-import ru.itmentor.spring.boot_security.demo.repositories.RoleRepository; 
+import ru.itmentor.spring.boot_security.demo.repositories.RoleRepository;
 import ru.itmentor.spring.boot_security.demo.repositories.UserRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.itmentor.spring.boot_security.demo.model.User;
+
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServicesImpl implements UserServices {
-    private final UserRepository userRepository; 
-    private final RoleRepository roleRepository; 
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;  
 
     @Autowired
     public UserServicesImpl(UserRepository userRepository, RoleRepository roleRepository) {
@@ -21,6 +24,7 @@ public class UserServicesImpl implements UserServices {
         this.roleRepository = roleRepository;
     }
 
+    @Transactional
     @Override
     public User findByUsername(String username) {
         return userRepository.findByUsername(username);
@@ -28,7 +32,8 @@ public class UserServicesImpl implements UserServices {
 
     @Override
     public User getUserById(Long id) {
-        return userRepository.findById(id).orElse(null);
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser.orElse(null); 
     }
 
     @Override
@@ -38,7 +43,7 @@ public class UserServicesImpl implements UserServices {
 
     @Override
     public void deleteUserById(Long id) {
-        userRepository.deleteById(id);
+        userRepository.deleteById(id); 
     }
 
     @Override
@@ -53,7 +58,53 @@ public class UserServicesImpl implements UserServices {
 
     @Override
     public List<User> findByNameOrEmail(String search) {
-        return userRepository.findByUsernameContainingIgnoreCaseOrEmailContainingIgnoreCase(search, search); 
-        
+        return userRepository.findByUsernameContainingOrEmailContaining(search, search);
+    }
+
+    @Transactional
+    @Override
+    public User createUser(User user) {
+     
+        userRepository.save(user); 
+        return user;
+    }
+
+    @Override
+    public User readUser(Long id) {
+        return getUserById(id);
+    }
+
+    @Transactional
+    @Override
+    public void updateUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Transactional
+    public User master(Long id, User updatedUser) {
+        updatedUser.setId(id);
+        userRepository.save(updatedUser); 
+        return updatedUser;
+    }
+
+    @Override
+    public List<User> getUsersAndRoles() {
+        return userRepository.listUsersAndRoles(); 
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return user; 
     }
 }
+
