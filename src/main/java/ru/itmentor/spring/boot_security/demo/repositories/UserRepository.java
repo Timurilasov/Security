5fb3c968fc7f9b535b90ebeae6601f8deb984195
Timurilasov <ilasovtimur08@gmail.com>
2024-10-29 10:43:35 +0300

    Update UserRepository.java

--- src/main/java/ru/itmentor/spring/boot_security/demo/repositories/UserRepository.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/repositories/UserRepository.java
@@ -6,13 +6,15 @@ import org.springframework.stereotype.Repository;
 import ru.itmentor.spring.boot_security.demo.model.User;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
-    @Query("SELECT u FROM User u JOIN FETCH u.roles r")
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles")
     List<User> listUsersAndRoles();
 
+    List<User> findByUsernameContainingOrEmailContaining(String username, String email);
 
+    User findByEmail(String email); 
 }
