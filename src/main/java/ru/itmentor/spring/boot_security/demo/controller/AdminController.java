427664fe93c912e2c84840b103c22866ebc576ce
Timurilasov <ilasovtimur08@gmail.com>
2024-10-29 10:47:40 +0300

    Update AdminController.java

--- src/main/java/ru/itmentor/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/controller/AdminController.java
@@ -1,50 +1,76 @@
-package ru.itmentor.spring.boot_security.demo.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import ru.itmentor.spring.boot_security.demo.model.Role;
-import ru.itmentor.spring.boot_security.demo.model.User;
-import ru.itmentor.spring.boot_security.demo.services.RoleService;
-import ru.itmentor.spring.boot_security.demo.services.UserService;
+import ru.itmentor.spring.boot_security.demo.services.UserServicesImpl;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
-@PreAuthorize("hasRole('ROLE_ADMIN')")
+@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 public class AdminController {
+    private final UserServicesImpl userServices;
+    private final RoleServiceImpl roleService;
 
-    private final UserService userService;
-    private final RoleService roleService;
-
-    @Autowired
-    public AdminController(UserService userService, RoleService roleService) {
-        this.userService = userService;
+    public AdminController(UserServicesImpl userServices, RoleServiceImpl roleService) {
+        this.userServices = userServices;
         this.roleService = roleService;
     }
 
-    @GetMapping("/add")
-    public String showAddUserForm(Model model) {
-        model.addAttribute("user", new User());
-        addAllRolesToModel(model); 
-        return "admin/addUser"; 
+    private void addAllRolesToModel(Model model) {
+        List<Role> roles = roleService.findAllRoles();
+        model.addAttribute("allRoles", roles);
     }
 
-    @PostMapping("/add")
-    public String addUser(User user) {
-        userService.saveUser(user); 
+    @GetMapping("")
+    public String adminHome(Model model, Authentication authentication) {
+        if (authentication != null) {
+            String adminName = authentication.getName();
+            model.addAttribute("adminName", adminName);
+        }
+        return "admin-home"; 
+    }
+
+    @GetMapping("/users")
+    public String showUserList(Model model) {
+        List<User> users = userServices.getUsersAndRoles();
+        model.addAttribute("users", users);
+        return "admin-all";
+    }
+
+    @GetMapping("/{id}")
+    public String readUser(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("user", userServices.readUser(id));
+        return "admin-show"; 
+    }
+
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        addAllRolesToModel(model);
+        model.addAttribute("user", new User());
+        return "admin-new"; 
+    }
+
+    @PostMapping("/new")
+    public String createUser(@ModelAttribute("user") User user) {
+        userServices.createUser(user);
         return "redirect:/admin/users";
     }
 
-    private void addAllRolesToModel(Model model) {
-        List<Role> roles = roleService.findAll(); 
-        model.addAttribute("roles", roles);
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable("id") Long id, Model model) {
+        User user = userServices.readUser(id);
+        addAllRolesToModel(model);
+        model.addAttribute("user", user);
+        return "admin-edit"; 
     }
 
-   
+    @PatchMapping("/{id}")
+    public String master(@ModelAttribute("user") User user, @PathVariable("id") Long id) {
+        userServices.master(id, user);
+        return "redirect:/admin/users";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userServices.deleteUser(id);
+        return "redirect:/admin/users";
+    }
 }
